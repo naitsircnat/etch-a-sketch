c34168e4362ac88a28fcd5b8a3c29c41ff32def8
Cristian <cristiangerardchen@gmail.com>
2023-06-29 17:35:26 +0800

    Complete all code, pending input limit of 100

---
+++ javascript.js
@@ -0,0 +1,57 @@
+function createGrids(numberOfGrids) {
+
+const containerElement=document.getElementById('gridsContainer');
+
+for (let i=0; i<numberOfGrids; i++) {
+    
+    const grid=document.createElement('div');
+
+    grid.style.border='1px solid black';
+
+    grid.style.width= (800/Math.sqrt(numberOfGrids)) + "px";
+
+    containerElement.appendChild(grid);
+
+    grid.classList.add("grid");
+
+    grid.addEventListener('mouseover', function handleMouseOver(){
+        grid.style.backgroundColor='black';
+    });
+
+}
+}
+
+createGrids(256);
+
+function removeElements() {
+   const grids=document.querySelectorAll('.grid');
+   grids.forEach(grid=> {
+    grid.remove();
+   })
+}
+
+const button=document.getElementById("question");
+
+button.addEventListener("click", function() {
+    var userInput=prompt ("How many squares per side?", 16)
+    var userInputInteger=parseInt(userInput);
+    numberOfGrids=userInputInteger*userInputInteger;
+    removeElements();
+    createGrids(numberOfGrids);
+}) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
